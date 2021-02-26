7fea61e6224c87371d0a7979b2a4ab4e205c009d
anonimak <anonimak@gmail.com>
2021-02-26 19:47:22 +0700

    update instal.sh

--- bash/install.sh
+++ bash/install.sh
@@ -3,7 +3,6 @@ DEPLOYPATH=/home/shfcoid/webtest.shf.co.id
 
 shopt -s extglob  # to enable extglob
 
-cp -rf !(index.php|storage) $DEPLOYPATH/public/*   $DEPLOYPATH/
-$DEPLOYPATH/php artisan storage:link
+cp -rf !(index.php) $DEPLOYPATH/public/*   $DEPLOYPATH/
 
 shopt -u extglob # disable extglob
