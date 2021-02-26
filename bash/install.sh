97484faef7436db722090df6530bad74ea8fbc7f
anonimak <anonimak@gmail.com>
2021-02-26 20:27:39 +0700

    update package.json

--- bash/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DEPLOYPATH=/home/shfcoid/webtest.shf.co.id
-
-shopt -s extglob  # to enable extglob
-
-cp -rf !(index.php) $DEPLOYPATH/public/*   $DEPLOYPATH/
-mv -f $DEPLOYPATH/index.php.server $DEPLOYPATH/index.php
-
-shopt -u extglob # disable extglob
