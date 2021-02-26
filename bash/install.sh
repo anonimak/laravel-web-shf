400515b354f5a2078ad47379fc7210ba32f925fd
Anonimak <sanonimak@gmail.com>
2021-02-26 15:40:18 +0700

    update bash script installation

---
+++ bash/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+DEPLOYPATH=/home/shfcoid/webtest.shf.co.id
+
+shopt -s extglob  # to enable extglob
+
+cp -rf !(index.php|storage) $DEPLOYPATH/public/*   $DEPLOYPATH/
+
+shopt -u extglob # disable extglob
