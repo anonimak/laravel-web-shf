c58c6b9e0520e60542440267c73a814ec08e83ea
Anonimak <sanonimak@gmail.com>
2021-02-26 16:44:57 +0700

    install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -4,5 +4,6 @@ DEPLOYPATH=/home/shfcoid/webtest.shf.co.id
 shopt -s extglob  # to enable extglob
 
 cp -rf !(index.php|storage) $DEPLOYPATH/public/*   $DEPLOYPATH/
+$DEPLOYPATH/php artisan storage:link
 
 shopt -u extglob # disable extglob
