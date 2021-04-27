5a9b6b32fc1cd325c7f2c130f7e71ff9c2a154e4
Anonimak <sanonimak@gmail.com>
2021-04-27 15:14:52 +0700

    update email to

--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -9,18 +9,18 @@ use Inertia\Inertia;
 class PengajuanController extends Controller
 {
     //
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         // kirim email
         $details = [
-            'fullname' => $request->input('firstname')." ".$request->input('lastname'),
+            'fullname' => $request->input('firstname') . " " . $request->input('lastname'),
             'category' => $request->input('category'),
             'email' => $request->input('email'),
             'telp' => $request->input('no_telp'),
             'text' => ($request->input('text')) ? $request->input('text') : '-',
         ];
-       
-        \Mail::to('jonatan.teofilus@gmail.com')->send(new \App\Mail\PengajuanMail($details));
+
+        \Mail::to($request->input('email'))->send(new \App\Mail\PengajuanMail($details));
         return Redirect::back()->with('success', 'Email sent.');
     }
-
 }
